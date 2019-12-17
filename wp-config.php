e4a92f371f20b628316d4d04bd90b1f8ba7a5417
thejojo974 <johanndumoulin734@gmail.com>
2019-12-17 13:19:01 +0100

    Arbo Dossier

--- wp-config.php
+++ wp-config.php
@@ -18,11 +18,6 @@
  * @package WordPress
  */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Content-type: text/html; charset=UTF-8'); 
-header('Access-Control-Allow-Headers: X-Requested-With');
-
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
