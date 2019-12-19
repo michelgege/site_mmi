51df86a6288d93e917a54a8d6de7c717d88c5d21
Etidou <45783736+Etidou@users.noreply.github.com>
2019-12-19 16:56:15 +0100

    ouai le debut

--- wp-content/themes/mmi/footer.php
+++ wp-content/themes/mmi/footer.php
@@ -1,13 +1,38 @@
 <footer>	
 <?php wp_footer()?>	
 
-<div id="wrap_footer">
+<div class="wrap_footer">
 
-	<img src="<?php the_field('twitter'); ?>" alt="Logo Twitter">
-	<img src="<?php the_field('facebook'); ?>" alt="Logo Facebook">
-	<img src="<?php the_field('instagram'); ?>" alt="Logo Instagram">
-<div id="rouge">jaieme le rouge</div>
+	<div id="logo">
+		<img src="<?php echo get_template_directory_uri() ?>/img/twitter.svg"  id="twitter" alt="Logo Twitter">
+		<img src="<?php echo get_template_directory_uri() ?>/img/facebook.svg"  id="facebook"   alt="Logo Facebook">
+		<img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" id="instagram"  alt="Logo Instagram">
+    </div>
+
+    <h3 id="adress"> 28 Avenue du Lac d'Annecy </br>
+    73370 Le Bourget-du-Lac </h3>
+
+    <div id="arrow">
+    	<img src="<?php echo get_template_directory_uri() ?>/img/arrow.svg" alt="arrow">
+    </div>
+
 </div>
+<div class="wrap_footer">
+
+    <div id="iut">
+    	<img src="<?php echo get_template_directory_uri() ?>/img/iut.svg" alt="iut">
+    </div>
+
+    <h3 id="secretary"> catherine.vandenbroucke@univ-smb.fr </br>
+    04 79 75 81 62 </h3>
+</div>
+
+
+
+
+
+
+
 </footer>
 
 </body>
