8813f97c6c0e9829d60f05889fca329547a0b208
Etidou <45783736+Etidou@users.noreply.github.com>
2019-12-19 16:04:50 +0100

    test

--- wp-content/themes/mmi/footer.php
+++ wp-content/themes/mmi/footer.php
@@ -1,5 +1,14 @@
 <footer>	
-	<?php wp_footer()?>
+<?php wp_footer()?>	
+
+<div id="wrap_footer">
+
+	<img src="<?php the_field('twitter'); ?>" alt="Logo Twitter">
+	<img src="<?php the_field('facebook'); ?>" alt="Logo Facebook">
+	<img src="<?php the_field('instagram'); ?>" alt="Logo Instagram">
+<div id="rouge">jaieme le rouge</div>
+</div>
 </footer>
+
 </body>
 </html>
