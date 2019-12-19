24f89e338fafcd07aa5833e31fcefef13d84fa71
Etidou <45783736+Etidou@users.noreply.github.com>
2019-12-19 18:52:01 +0100

    Footer fini

--- wp-content/themes/mmi/footer.php
+++ wp-content/themes/mmi/footer.php
@@ -1,31 +1,44 @@
 <footer>	
-<?php wp_footer()?>	
+    <?php wp_footer()?>	
 
 <div class="wrap_footer">
+    <div id="top">
+        <div class="left">
+              <img src="<?php echo get_template_directory_uri() ?>/img/twitter.svg"  id="twitter" alt="Logo Twitter">
+              <img src="<?php echo get_template_directory_uri() ?>/img/facebook.svg"  id="facebook"   alt="Logo Facebook">
+              <img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" id="instagram"  alt="Logo Instagram">
+        </div>
 
-	<div id="logo">
-		<img src="<?php echo get_template_directory_uri() ?>/img/twitter.svg"  id="twitter" alt="Logo Twitter">
-		<img src="<?php echo get_template_directory_uri() ?>/img/facebook.svg"  id="facebook"   alt="Logo Facebook">
-		<img src="<?php echo get_template_directory_uri() ?>/img/instagram.svg" id="instagram"  alt="Logo Instagram">
-    </div>
+        <h3 class="center"> 28 Avenue du Lac d'Annecy </br>
+            73370 Le Bourget-du-Lac 
+        </h3>
 
-    <h3 id="adress"> 28 Avenue du Lac d'Annecy </br>
-    73370 Le Bourget-du-Lac </h3>
+        <div class="right">
+           <img src="<?php echo get_template_directory_uri() ?>/img/arrow.svg" alt="arrow">
+        </div>
 
-    <div id="arrow">
-    	<img src="<?php echo get_template_directory_uri() ?>/img/arrow.svg" alt="arrow">
+   </div>
+
+
+   <div id="bottom">
+        <div class="left">
+            <img src="<?php echo get_template_directory_uri() ?>/img/iut.svg" alt="iut">
+        </div>
+
+        <h3 class="centerbot"> catherine.vandenbroucke@univ-smb.fr </br>
+        04 79 75 81 62 </br> </br>
+        <span class="grey">© MMI Chambéry - Tous droits réservés - Mentions légales </span>
+        </h3>
+
+        <div class="right">
+            <img src="<?php echo get_template_directory_uri() ?>/img/mmi_footer.svg" alt="iut">
+        </div>
     </div>
 
 </div>
-<div class="wrap_footer">
 
-    <div id="iut">
-    	<img src="<?php echo get_template_directory_uri() ?>/img/iut.svg" alt="iut">
-    </div>
 
-    <h3 id="secretary"> catherine.vandenbroucke@univ-smb.fr </br>
-    04 79 75 81 62 </h3>
-</div>
+
 
 
 
