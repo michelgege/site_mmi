c027b3d766dc0122fe5210383991ed6ee04a40e2
michelgege <50527053+michelgege@users.noreply.github.com>
2019-12-23 16:05:02 +0100

    realisations 50%

--- wp-content/themes/mmi/functions.php
+++ wp-content/themes/mmi/functions.php
@@ -20,8 +20,11 @@
 		
         // SCRIPT
         wp_enqueue_script('coin_pro', get_template_directory_uri().'/js/libs/jquery.js');
-        wp_enqueue_script('coin_pro', get_template_directory_uri().'/js/libs/parallax.min.js');
+	  wp_enqueue_script('coin_pro', get_template_directory_uri().'/js/libs/parallax.min.js');
+	  wp_enqueue_script('realisations', get_template_directory_uri().'/js/libs/jquery.js');
+        wp_enqueue_script('realisations', get_template_directory_uri().'/js/libs/parallax.min.js');
 		wp_enqueue_script('coin_pro', get_template_directory_uri().'/js/coin_pro.js');
+		wp_enqueue_script('realisations', get_template_directory_uri().'/js/coin_pro.js');
 		
 		
 		//Load Styles enqueued
