669f1389da35e754e7bf5ca82d72a8a5ba146887
thejojo974 <johanndumoulin734@gmail.com>
2019-12-23 13:08:21 +0100

    Update

--- wp-content/themes/mmi/index.php
+++ wp-content/themes/mmi/index.php
@@ -8,7 +8,18 @@
 	get_header('index');
 ?>
 
-<p>Index</p>
+<div class="wrap">
+	<section id="skills">
+		<h2>LES COMPÉTENCES</h2>
+		<div id="skills_content">
+			<p>Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles. Mais s'est aussi adapté à la bureautique.</p>
+			<div id="skills_graphic">
+				
+			</div>
+		</div>
+		<p>MMI en détails</p>
+	</section>
+</div>
 
 <?php
 	get_footer();
