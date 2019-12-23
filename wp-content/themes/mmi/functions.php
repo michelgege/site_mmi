26b0ed62700a6603bcf2babf2a100c5cdc961f9d
thejojo974 <johanndumoulin734@gmail.com>
2019-12-23 15:06:00 +0100

    Update index

--- wp-content/themes/mmi/functions.php
+++ wp-content/themes/mmi/functions.php
@@ -6,7 +6,6 @@
 		wp_enqueue_style('header', get_template_directory_uri().'/css/header.css');
 		wp_enqueue_style('footer', get_template_directory_uri().'/css/footer.css');
 		wp_enqueue_style('raleway', get_template_directory_uri().'https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800&display=swap');
-		wp_enqueue_style('raleway', get_template_directory_uri()."https://fonts.googleapis.com/css?family=Oswald:700&display=swap");
 
 		//Styles enqueued
 		wp_register_style('index', get_template_directory_uri().'/css/index.css');
