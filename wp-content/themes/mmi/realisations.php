de83ac88749b10e9ed38bac2c81ee045f9de589a
michelgege <50527053+michelgege@users.noreply.github.com>
2019-12-24 13:40:56 +0100

    popup70%

--- wp-content/themes/mmi/realisations.php
+++ wp-content/themes/mmi/realisations.php
@@ -199,29 +199,32 @@
 <div class="pop_up">
 	
 	<div class="background">
-		<div class="wrap">
-			<div class="inner_wrap">
+	</div>
+	<div class="wrap">
+		<div class="inner_wrap">
 
-				<div class="content">
-					<div class="realisation">
-						<img src="http://placehold.jp/300x300.jpg" alt="">
-						<a> Voir la realisation</a>
+			<div class="content">
+				<div class="realisation">
+					<img src="http://placehold.jp/300x300.jpg" alt="">
+					<a>Voir la realisation</a>
+				</div>
+	
+				<div class="description">
+					<div>
+						<h3 class="domain">Graphisme</h3>
+						<p class="date">2008</p>
 					</div>
-		
-					<div class="description">
+					<div>
+						<p class="author_and_name">"Lorem Ipsum" par <span class="author_bold">John Doe</span></p>
 						<div>
-							<h3 class="domain">Graphisme</h3>
-							<p class="date">2008</p>
-						</div>
-						<div>
-							<p class="author_and_name">"Lorem Ipsum" par <span class="author_bold"></span></p>
 							<a><img src="<?php echo get_template_directory_uri() ?>/img/realisations/pinterest.png"/></a>
 							<a><img src="<?php echo get_template_directory_uri() ?>/img/realisations/linkedin.png"/></a>
 						</div>
-						<p>Réalisé pour <a href="http://www.mairie-lamotteservolex.fr/">la Mairie de la Motte-Servolex</a></p>
-						<p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci eius consectetur rem facilis, ad necessitatibus quia quaerat assumenda, at quisquam, labore alias ipsa suscipit? Enim exercitationem cupiditate nulla assumenda excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id repellat nemo aliquid placeat quis, exercitationem quas, perspiciatis fugiat deserunt labore dolore error voluptatem ipsa ut incidunt facilis soluta velit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum ut nihil ad libero at quas error minus, aperiam odio suscipit impedit vero ullam vitae, iste ducimus. Distinctio ipsum porro aut.</p>
-		
 					</div>
+					<p>Réalisé pour <a target="_blank" href="http://www.mairie-lamotteservolex.fr/">la Mairie de la Motte-Servolex</a></p>
+
+					<p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci eius consectetur rem facilis, ad necessitatibus quia quaerat assumenda, at quisquam, labore alias ipsa suscipit? Enim exercitationem cupiditate nulla assumenda excepturi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id repellat nemo aliquid placeat quis, exercitationem quas, perspiciatis fugiat deserunt labore dolore error voluptatem ipsa ut incidunt facilis soluta velit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum ut nihil ad libero at quas error minus, aperiam odio suscipit impedit vero ullam vitae, iste ducimus. Distinctio ipsum porro aut.</p>
+	
 				</div>
 			</div>
 		</div>
