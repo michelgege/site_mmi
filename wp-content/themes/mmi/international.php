2cdbaf2200baae044f00309e7608376f3b6f4516
thejojo974 <johanndumoulin734@gmail.com>
2020-01-11 12:25:04 +0100

    Update Carte JS

--- wp-content/themes/mmi/international.php
+++ wp-content/themes/mmi/international.php
@@ -2269,19 +2269,4 @@
 
 	<?php
 		get_footer();
-	?>
-
-<script>
-  jQuery(document).ready(function() {
-	jQuery(".pins").click(function() {
-
-		jQuery(this).addClass('active');
-		jQuery(".pins").not(this).removeClass('active');
-
-		var id=jQuery(this).attr("data-id");
-		jQuery('.pop_up[data-id= "'+id+'"]').addClass('active');
-		jQuery(".pop_up").not('.pop_up[data-id= "'+id+'"]').removeClass('active');
-	})
-  });	
-</script>
-	
+	?>	
