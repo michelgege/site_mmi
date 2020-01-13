46ca2460f1728d817a461a27aa70adc0f2d07ac6
paulliiiine <57755343+paulliiiine@users.noreply.github.com>
2020-01-13 07:03:10 +0100

    etapres

--- wp-content/themes/mmi/et_apres.php
+++ wp-content/themes/mmi/et_apres.php
@@ -12,6 +12,47 @@
 
 <section id="testimony">
 	<h2></h2>
+	<div>
+		<div>
+			<div>
+				<img src="img/et_apres/testimony.jpg" alt="photo de profile">
+				<div>
+					<h3>Catherine BELCOUR</h3>
+					<h4>Responsable marketing et communication</h4>
+					<p>Clean Car, à Chambéry</p>
+				</div>
+				<p>La licence professionnelle c'est : Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse </p>
+			</div>
+			<div>
+				<img src="img/et_apres/testimony.jpg" alt="photo de profile">
+				<div>
+					<h3>Catherine BELCOUR</h3>
+					<h4>Responsable marketing et communication</h4>
+					<p>Clean Car, à Chambéry</p>
+				</div>
+				<p>La licence professionnelle c'est : Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse </p>
+			</div>
+			<div>
+				<img src="img/et_apres/testimony.jpg" alt="photo de profile">
+				<div>
+					<h3>Catherine BELCOUR</h3>
+					<h4>Responsable marketing et communication</h4>
+					<p>Clean Car, à Chambéry</p>
+				</div>
+				<p>La licence professionnelle c'est : Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse </p>
+			</div>
+			<div>
+				<img src="img/et_apres/testimony.jpg" alt="photo de profile">
+				<div>
+					<h3>Catherine BELCOUR</h3>
+					<h4>Responsable marketing et communication</h4>
+					<p>Clean Car, à Chambéry</p>
+				</div>
+				<p>La licence professionnelle c'est : Haec igitur lex in amicitia sanciatur, ut neque rogemus res turpes nec faciamus rogati. Turpis enim excusatio est et minime accipienda cum in ceteris peccatis, tum si quis contra rem publicam se amici causa fecisse </p>
+			</div>
+		</div>
+		<input type="button" name="plus" value="Voir Plus De Métier">
+	</div>
 
 
 </section>
