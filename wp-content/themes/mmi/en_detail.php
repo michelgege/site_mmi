2629267dfd59211649765315499ab358d1f57723
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-15 14:15:58 +0100

    ok

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php
-	get_header();
+get_header();
 ?>
 
 <p>En détails</p>
@@ -13,15 +13,46 @@
 <div id="wrap">
 
 
+	<div id="destiny">
+		<h2 class="right_title"> A qui est destiné le BUT MMI ? </h2>
+		<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
+		<p class="content_right">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.</p>
+		<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
+		<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
+	</div>
 
-<h2 class="right_title"> A qui est destiné le BUT MMI ? </h2>
-<p class="content"> Notre objectif est de composer une promotion avec : </p>
-<p class="content">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.
--	50% de bacheliers généraux, avec des options scientifiques.</p>
-<p class="content">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
+
+	<h2 class="left_title"> Les projets </h2>
+	<p class="content_left">Les projets sont au coeur de la formation MMI. Ils permettent aux étudiants de mettre 
+		en application pratique des savoirs théoriques. Grâce aux projets tuteurés, les étudiants 
+		peuvent présenter des réalisations de qualité pour leur devenir professionnel (stage, 
+	alternance ou emploi).</p>
+
+	<div id="wrap_year">
+		<div id="number_one">
+			<h2 class="year"> Première année </h2>
+			<p class="content_center">Réalisation et mise en oeuvre d’une exposition multimédia : conception des oeuvres et des outils de communication relatifs à l’exposition. </p>
+		</div>
+		<div id="number_two">
+			<h2 class="year"> Deuxième année </h2>
+			<p class="content_center">Constitués en agences, les étudiants répondent à un besoin réel issu d’un client : création de sites web, conception de produits audiovisuels, stratégie de communication… </p>
+		</div>
+		<div id="number_three">
+			<h2 class="year"> Troisième année </h2>
+			<p class="content_center">Stage en entreprise et possibilité d'alternance </p>
+		</div>
+	</div>
+
+	<div id="after_content_purple">
+		<h2 class="right_title"> La spécialité de MMI Chambéry </h2>
+		<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
+		<p class="content_right">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.</p>
+		<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
+		<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
+	</div>
 </div>
 
 
 <?php
-	get_footer();
+get_footer();
 ?>
