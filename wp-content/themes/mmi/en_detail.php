0c6563659718b3ccd52cd6d7bbdc16ed9b6ba465
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-15 14:57:12 +0100

    ok

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -11,58 +11,8 @@ get_header();
 <p>En détails</p>
 
 <div id="wrap">
-<<<<<<< HEAD
- 
-<section class="le_but">
-	<h1>Le BUT</h1>
-
-	<h2 class="right_title">Les compétences</h2>
-	<p class="competences">Le <span>Bachelor Universitaire de Technologie</span> (BUT) « Métiers du Multimédia et de l’Internet » (MMI) se déroule en <span>3 ans</span>, découpés en <span>6 semestres</span>, sur un volume total de <span>2000 heures de cours</span> en présence d’un enseignant. A cela s’ajoutent 600 heures de projet réparties sur les 3 années.</p>
-
-	<p>Chaque étudiant doit valider 5 blocs de compétences pour l’obtention du BUT MMI découpés de la manière suivante :</p>
-
-</section>>
-=======
-
->>>>>>> 28a581f3f1b84b92c026446d659f36601dc0db06
-
-<<<<<<< HEAD
-	<<<<<<< HEAD
-
-	<h2 class="sub_title"> A qui est destiné le BUT MMI ? </h2>
 
 
-	<section class="departement">
-		<h1>Le département</h1>
-		<div class="intro">
-			<p >Nous vous accueillons sur le site du Bourget du Lac dans des locaux équipés de matériel informatique et 
-			audiovisuel récent que les étudiants peuvent emprunter.</p>
-			<div class="video">
-				<div class="button">
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/playbutton.svg">
-				</div>
-				<video src="" poster="http://localhost/site_mmi/wp-content/themes/mmi/img/accueil/slider2_1.jpg">
-				</div>
-			</div>
-		</section>
-		<section>
-			
-		</section>
-		=======
-		<div id="destiny">
-			<h2 class="right_title"> A qui est destiné le BUT MMI ? </h2>
-			<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
-			<p class="content_right">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.</p>
-			<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
-			<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
-		</div>
-=======
-<<<<<<< HEAD
-
-<h2 class="sub_title"> A qui est destiné le BUT MMI ? </h2>
-
-
-=======
 	<div id="destiny">
 		<h2 class="right_title"> A qui est destiné le BUT MMI ? </h2>
 		<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
@@ -70,71 +20,42 @@ get_header();
 		<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
 		<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
 	</div>
->>>>>>> ea829cfc3a0a504be9e2421dc2eb66d79f1f2ffa
 
 
-		<h2 class="left_title"> Les projets </h2>
-		<p class="content_left">Les projets sont au coeur de la formation MMI. Ils permettent aux étudiants de mettre 
-			en application pratique des savoirs théoriques. Grâce aux projets tuteurés, les étudiants 
-			peuvent présenter des réalisations de qualité pour leur devenir professionnel (stage, 
-		alternance ou emploi).</p>
+	<h2 class="left_title"> Les projets </h2>
+	<p class="content_left">Les projets sont au coeur de la formation MMI. Ils permettent aux étudiants de mettre 
+		en application pratique des savoirs théoriques. Grâce aux projets tuteurés, les étudiants 
+		peuvent présenter des réalisations de qualité pour leur devenir professionnel (stage, 
+	alternance ou emploi).</p>
 
-		<div id="wrap_year">
-			<div id="number_one">
-				<h2 class="year"> Première année </h2>
-				<p class="content_center">Réalisation et mise en oeuvre d’une exposition multimédia : conception des oeuvres et des outils de communication relatifs à l’exposition. </p>
-			</div>
-			<div id="number_two">
-				<h2 class="year"> Deuxième année </h2>
-				<p class="content_center">Constitués en agences, les étudiants répondent à un besoin réel issu d’un client : création de sites web, conception de produits audiovisuels, stratégie de communication… </p>
-			</div>
-			<div id="number_three">
-				<h2 class="year"> Troisième année </h2>
-				<p class="content_center">Stage en entreprise et possibilité d'alternance </p>
-			</div>
+	<div id="wrap_year">
+		<div id="number_one">
+			<h2 class="year"> Première année </h2>
+			<p class="content_center">Réalisation et mise en oeuvre d’une exposition multimédia : conception des oeuvres et des outils de communication relatifs à l’exposition. </p>
 		</div>
-
-		<div id="after_content_purple">
-			<h2 class="right_title"> La spécialité de MMI Chambéry </h2>
-			<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
-			<p class="content_right">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.</p>
-			<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
-			<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
+		<div id="number_two">
+			<h2 class="year"> Deuxième année </h2>
+			<p class="content_center">Constitués en agences, les étudiants répondent à un besoin réel issu d’un client : création de sites web, conception de produits audiovisuels, stratégie de communication… </p>
 		</div>
-
+		<div id="number_three">
+			<h2 class="year"> Troisième année </h2>
+			<p class="content_center">Stage en entreprise et possibilité d'alternance </p>
+		</div>
 	</div>
-<<<<<<< HEAD
-=======
->>>>>>> 0b0180a39e50c63df275a547fa4e76d59dfde7db
-	<section class="departement">
-		<h1>Le département</h1>
-		<div class="intro">
-			<p >Nous vous accueillons sur le site du Bourget du Lac dans des locaux équipés de matériel informatique et 
-	audiovisuel récent que les étudiants peuvent emprunter.</p>
-			<div class="video">
-				<div class="button">
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/playbutton.svg">
-				</div>
-				<video src="" poster="http://localhost/site_mmi/wp-content/themes/mmi/img/accueil/slider2_1.jpg">
-			</div>
-		</div>
-	</section>
-	<section>
-		
-	</section>
-</div> 
->>>>>>> ea829cfc3a0a504be9e2421dc2eb66d79f1f2ffa
+
+	<div id="after_content_purple">
+		<h2 class="right_title"> La spécialité de MMI Chambéry </h2>
+		<p class="content_right"> Notre objectif est de composer une promotion avec : </p>
+		<p class="content_right">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.</p>
+		<p class="content_right">-	50% de bacheliers généraux, avec des options scientifiques.</p>
+		<p class="content_right">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
+	</div>
+
+</div>
 
 
 
-
-<<<<<<< HEAD
-	<?php
-	get_footer();
-	?>
-=======
 
 <?php
 get_footer();
 ?>
->>>>>>> ea829cfc3a0a504be9e2421dc2eb66d79f1f2ffa
