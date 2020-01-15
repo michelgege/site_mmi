38104986dd5a1d7c0f19bdb27d069614bb1bf3d9
Megathony <Megathony@users.noreply.github.com>
2020-01-15 14:17:15 +0100

    Antho

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -15,7 +15,27 @@
 
 
 <h2 class="sub_title"> A qui est destiné le BUT MMI ? </h2>
+
+
+<section class="departement">
+	<h1>Le département</h1>
+	<div class="intro">
+		<p >Nous vous accueillons sur le site du Bourget du Lac dans des locaux équipés de matériel informatique et 
+audiovisuel récent que les étudiants peuvent emprunter.</p>
+		<div class="video">
+			<div class="button">
+				<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/playbutton.svg">
+			</div>
+			<video src="" poster="http://localhost/site_mmi/wp-content/themes/mmi/img/accueil/slider2_1.jpg">
+		</div>
+	</div>
+</section>
+<section>
+	
+</section>
 </div>
+
+
 
 
 <?php
