8c5de4547771577228c4681a0bb2a3a2ef50c5ea
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-15 13:39:26 +0100

    push pur classe

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -14,7 +14,11 @@
 
 
 
-<h2 class="sub_title"> A qui est destiné le BUT MMI ? </h2>
+<h2 class="right_title"> A qui est destiné le BUT MMI ? </h2>
+<p class="content"> Notre objectif est de composer une promotion avec : </p>
+<p class="content">-	50% de bacheliers technologiques, de préférence issus de la filière STI2D option SIN, avec un excellent dossier.
+-	50% de bacheliers généraux, avec des options scientifiques.</p>
+<p class="content">Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture artistique et esthétique. </p>
 </div>
 
 
