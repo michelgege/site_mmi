ba7a5903ef56ee46122f310253ea225b8a2d78ef
thejojo974 <johanndumoulin734@gmail.com>
2020-01-15 10:30:59 +0100

    Update

--- wp-content/themes/mmi/header.php
+++ wp-content/themes/mmi/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes();?>>
 <head>
 	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<title><?php the_title(); ?></title>
 	<?php wp_head();?>
 </head>
