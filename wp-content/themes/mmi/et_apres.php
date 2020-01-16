bfa352226783cb18e2a596b19753ba69a1382df2
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-16 08:10:37 +0100

    ush

--- wp-content/themes/mmi/et_apres.php
+++ wp-content/themes/mmi/et_apres.php
@@ -13,19 +13,19 @@
 	<div id="scene" class="scene">
 
 		<div data-depth="-1.80">
-			<img class="empty_top_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/empty_top_triangle" alt=""> 
+			<img class="empty_top_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/empty_top_triangle.svg" alt=""> 
 		</div>
 
 		<div data-depth="2.50">
-			<img class="green_top_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/green_top_triangle" alt="">
+			<img class="green_top_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/green_top_triangle.svg" alt="">
 		</div>
 
 		<div data-depth="-2.25">
-			<img class="grey_square" src="<?php echo get_template_directory_uri() ?>/img/et_apres/grey_square" alt="">
+			<img class="grey_square" src="<?php echo get_template_directory_uri() ?>/img/et_apres/grey_square.svg" alt="">
 		</div>
 
 		<div data-depth="1.25">
-			<img class="side_green_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/side_green_triangle" alt="">
+			<img class="side_green_triangle" src="<?php echo get_template_directory_uri() ?>/img/et_apres/side_green_triangle.svg" alt="">
 		</div>
 
 	</div>
