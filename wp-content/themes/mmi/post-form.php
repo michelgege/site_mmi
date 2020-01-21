42026ccf81aa9fadcced7d0f880510ca954bcf52
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-21 11:22:47 +0100

    OK

--- wp-content/themes/mmi/post-form.php
+++ wp-content/themes/mmi/post-form.php
@@ -1,6 +1,6 @@
 	<?php
 /**
- * Template Name: Coin Pro
+ * Template Name: Post-form
  */
 ?>
 
@@ -8,13 +8,16 @@
 get_header();
 ?>
 
-
-
 	<div class="pop-up">
 		<img src="<?php echo get_template_directory_uri() ?>/img/coin_pro/cross.svg" data-depth="" id="cross" alt="cross">
 		<h1 id="title_pop"> Merci pour ces renseignements ! </h1>
 		<p id="content"> Le formulaire te sera envoyé d'ici quelques jours. En attendant, n'hésite pas à consulter notre site ou à te rendre sur les réseaux sociaux. </p>
 		<img src="<?php echo get_template_directory_uri() ?>/img/coin_pro/plane.svg" data-depth="" id="plane" alt="plane">
+        <div id="flex">
+        	<p id="form"> Votre formulaire a bien été envoyé, veuillez attendre</p> <h3 id="counter">9000</h3> <p id="sec"> secondes </p>
+        </div>
+
+        <a href="../coin_pro#wrapform" id="rea"> <input type="button" class="button" href="../coin_pro#wrapform" value="Retour"> </a>
 
 	</div>
 
