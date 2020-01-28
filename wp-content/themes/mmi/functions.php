15af9a26775d09bb463210be3b2a52a6d10829f5
Etidou <45783736+Etidou@users.noreply.github.com>
2020-01-28 10:19:12 +0100

    NORMALEMENT C'EST UPDATE

--- wp-content/themes/mmi/functions.php
+++ wp-content/themes/mmi/functions.php
@@ -18,6 +18,7 @@
 		wp_register_style('header_index', get_template_directory_uri().'/css/header_index.css');
 
 		wp_register_style('coin_pro', get_template_directory_uri().'/css/coin_pro.css');
+		wp_register_style('responsive_coin_pro', get_template_directory_uri().'/css/responsive_coin_pro.css');
 		wp_register_style('en_detail', get_template_directory_uri().'/css/en_detail.css');
 		wp_register_style('responsive_en_details', get_template_directory_uri().'/css/responsive_en_details.css');
 		wp_register_style('et_apres', get_template_directory_uri().'/css/et_apres.css');
@@ -85,8 +86,8 @@
 
 		else if (is_page('mmi-en-detail')) {
 			wp_enqueue_script('en_detail_js');
-			wp_enqueue_style('responsive_en_details');
 			wp_enqueue_style('en_detail');
+			wp_enqueue_style('responsive_en_details');
 		}
 
 		else if (is_page('realisations')) {
