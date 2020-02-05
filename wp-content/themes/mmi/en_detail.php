2d2b2b91175f92d6bd4d33f24b317ca530dcf20d
paulliiiine <57755343+paulliiiine@users.noreply.github.com>
2020-02-05 21:52:49 +0100

    Update en_detail.php

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -22,9 +22,9 @@ get_header();
 		<h2>Le BUT</h2>
 		<img class="tablet" src="<?php echo get_template_directory_uri() ?>/img/en_detail/tablet.svg" alt="tablette graphique">
 		<h3 class="left_title"><?php the_field('detail_first_title'); ?></h3>
-		<p class="competences_1">Le <span>Bachelor Universitaire de Technologie</span> (BUT) « Métiers du Multimédia et de l’Internet » (MMI) se déroule en <span>3 ans</span>, découpés en <span>6 semestres</span>, sur un volume total de <span>2000 heures de cours</span> en présence d’un enseignant. A cela s’ajoutent 600 heures de projet réparties sur les 3 années.</p>
+		<p class="competences_1"><?php the_field('detail_description_but'); ?></p>
 
-		<p class="competences_2">Chaque étudiant doit valider 5 blocs de compétences pour l’obtention du BUT MMI découpés de la manière suivante :</p>
+		<p class="competences_2"><?php the_field('detail_intro_competence'); ?>:</p>
 
 		<img class="checklist" src="<?php echo get_template_directory_uri() ?>/img/en_detail/checklist.svg" alt="checklist">
 		<img class="camera" src="<?php echo get_template_directory_uri() ?>/img/en_detail/camera.svg" alt="camera">
@@ -33,27 +33,27 @@ get_header();
 			<div class="competence">
 				<div class="titre" data-id="1">
 					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de produire du contenu multimédia</h4>
+					<h4><?php the_field('detail_title_competence_1'); ?></h4>
 				</div>
 				<div class="content" data-id="1">
-					<p>- en respectant les contraintes techniques du milieu professionnel en vigueur</p>
-					<p>- en considérant les normes de numérisation et leur impact</p>
-					<p>- en sachant choisir et utiliser le matériel et logiciels adéquats</p>
-					<p>- en faisant preuve d'invention et de créativité dans l'utilisation des outils</p>
-					<p>- en mettant en œuvre les fondamentaux des langages plastiques et filmiques</p>
+					<p>- <?php the_field('detail_develop_competence_1.1'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_1.2'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_1.3'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_1.4'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_1.5'); ?></p>
 				</div>
 			</div>
 
 			<div class="competence">
 				<div class="titre" data-id="2">
 					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de programmer pour le web</h4>
+					<h4><?php the_field('detail_title_competence_2'); ?></h4>
 				</div>
 				<div class="content" data-id="2">
-					<p>- en étant capable de s'adapter aux différentes technologies</p>
-					<p>- en produisant en mettant en œuvre des algorithmes répondant aux problématiques</p>
-					<p>- en respectant les normes et standards du web</p>
-					<p>- en adaptant la présentation du contenu au support et aux utilisateurs</p>
+					<p>- <?php the_field('detail_develop_competence_2.1'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_2.2'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_2.3'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_2.4'); ?></p>
 				</div>
 
 			</div>
@@ -61,39 +61,39 @@ get_header();
 			<div class="competence">
 				<div class="titre" data-id="3">
 					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de mettre en place et administrer des systèmes d'information</h4>
+					<h4><?php the_field('detail_title_competence_3'); ?></h4>
 				</div>
 				<div class="content" data-id="3">
-					<p>- en s'appuyant sur des modèles existants</p>
-					<p>- en respectant les contraintes de légalité et de sécurité</p>
-					<p>- en utilisant des logiciels et outils de gestion de base de données</p>
-					<p>- en gérant et mettant en œuvre une configuration matérielle et logicielle locale ou en réseau</p>
+					<p>- <?php the_field('detail_develop_competence_3.1'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_3.2'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_3.3'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_3.4'); ?></p>
 				</div>
 			</div>
 
 			<div class="competence">
 				<div class="titre" data-id="4">
 					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de gérer un projet numérique</h4>
+					<h4><?php the_field('detail_title_competence_4'); ?></h4>
 				</div>
 				<div class="content" data-id="4">
-					<p>- en concevant et préparant la mise en œuvre du projet</p>
-					<p>- en réceptionnant la demande</p>
-					<p>- en ayant une stratégie d'organisation du travail</p>
-					<p>- en développant une communication adaptée à la conduite du projet</p>
+					<p>- <?php the_field('detail_develop_competence_4.1'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_4.2'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_4.3'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_4.4'); ?></p>
 				</div>
 			</div>
 
 			<div class="competence">
 				<div class="titre" data-id="5">
 					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable d'élaborer et rédiger des contenus </h>
+					<h4><?php the_field('detail_title_competence_5'); ?></h>
 				</div>
 				<div class="content" data-id="5">
-					<p>- en déterminant les enjeux, les cibles et les objectifs de communication adéquats</p>
-					<p>- en mobilisant les univers de sens pertinents</p>
-					<p>- en concevant des contenus adaptés à des supports divers et variés</p>
-					<p>- en respectant une stratégie de communication</p>
+					<p>- <?php the_field('detail_develop_competence_5.1'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_5.2'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_5.3'); ?></p>
+					<p>- <?php the_field('detail_develop_competence_5.4'); ?></p>
 				</div>
 			</div>
 
