e6430e78ac3907ada23a83e1582da08080792fe6
thejojo974 <johanndumoulin734@gmail.com>
2020-02-25 11:51:48 +0100

    Inté en détail + fields fini

--- wp-content/themes/mmi/en_detail.php
+++ wp-content/themes/mmi/en_detail.php
@@ -11,167 +11,67 @@ get_header();
 	<section id="but">
 		<h2>Le BUT</h2>
 
+		<?php $detail_but = get_field('detail_but', $post->ID); ?>
 
 		<div class="container right">
 			<h3>Les compétences</h3>
-			<!-- <p>Le Bachelor Universitaire de Technologie (BUT) « Métiers du Multimédia et de l’Internet » (MMI) se déroule en 3 ans, découpés en 6 semestres, sur un volume total de 2000 heures de cours en présence d’un enseignant. A cela s’ajoutent 600 heures de projet réparties sur les 3 années.</p> -->
-			<?php $test = get_field('detail_but')['detail_but_skills_content']; var_dump($test); ?>
+			<?php echo($detail_but['detail_but_skills_content']); ?>
 			<img class="icon" src="<?php echo get_template_directory_uri() ?>/img/en_detail/tablet.svg" alt="">
 		</div>
 
 		<p>Chaque étudiant doit valider 5 blocs de compétences pour l’obtention du BUT MMI découpés de la manière suivante :</p>
 
 		<div id="skills">
-			<!-- <?php while( have_rows('detail_skills') ): the_row(); 
+			<?php foreach($detail_but['detail_but_skills_list'] as $title): ?>
 
-				// vars
-				$image = get_sub_field('detail_team_img');
-				$name = get_sub_field('detail_team_name');
-				$job = get_sub_field('detail_team_job');
-				$testimony = get_sub_field('detail_team_testimony');
+				<div class="skills_container">
+					<div>
+						<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
+						<h4><?php echo($title['detail_but_skills_list_title']); ?></h4>
+					</div>
+					<ul>
 
-				?>
+						<?php foreach($title['detail_but_skills_list_second'] as $item): ?> 
 
-				
+							<li><?php echo($item['detail_but_skills_list_second_item']); ?></li>
 
-			<?php endwhile; ?> -->
-			<div class="skills_container">
-				<div>
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de produire du contenu multimédia</h4>
+						<?php endforeach; ?>
+
+					</ul>
 				</div>
-				<ul>
-					<li>en respectant les contraintes techniques du milieu professionnel en vigueur</li>
-					<li>en considérant les normes de numérisation et leur impact</li>
-					<li>en sachant choisir et utiliser le matériel et logiciels adéquats</li>
-					<li>en faisant preuve d'invention et de créativité dans l'utilisation des outils</li>
-					<li>en mettant en œuvre les fondamentaux des langages plastiques et filmiques</li>
-				</ul>
-			</div>
-			<div class="skills_container">
-				<div>
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de programmer pour le web</h4>
-				</div>
-				<ul>
-					<li>en étant capable de s'adapter aux différentes technologies</li>
-					<li>en produisant en mettant en œuvre des algorithmes répondant aux problématiques</li>
-					<li>en respectant les normes et standards du web</li>
-					<li>en adaptant la présentation du contenu au support et aux utilisateurs</li>
-				</ul>
-			</div>	
-			<div class="skills_container">
-				<div>
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de mettre en place et administrer des systèmes d'information</h4>
-				</div>
-				<ul>
-					<li>en s'appuyant sur des modèles existants</li>
-					<li>en respectant les contraintes de légalité et de sécurité</li>
-					<li>en utilisant des logiciels et outils de gestion de base de données</li>
-					<li>en gérant et mettant en œuvre une configuration matérielle et logicielle locale ou en réseau</li>
-				</ul>
-			</div>	
-			<div class="skills_container">
-				<div>
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable de gérer un projet numérique</h4>
-				</div>
-				<ul>
-					<li>en concevant et préparant la mise en œuvre du projet</li>
-					<li>en réceptionnant la demande</li>
-					<li>en ayant une stratégie d'organisation du travail</li>
-					<li>en développant une communication adaptée à la conduite du projet</li>
-				</ul>
-			</div>	
-			<div class="skills_container">
-				<div>
-					<img src="<?php echo get_template_directory_uri() ?>/img/en_detail/plus.svg" alt="plus">
-					<h4>Savoir et être capable d'élaborer et rédiger des contenus </h4>
-				</div>
-				<ul>
-					<li>en déterminant les enjeux, les cibles et les objectifs de communication adéquats</li>
-					<li>en mobilisant les univers de sens pertinents</li>
-					<li>en concevant des contenus adaptés à des supports divers et variés</li>
-					<li>en respectant une stratégie de communication</li>
-				</ul>
-			</div>
+
+			<?php endforeach; ?>
 			<img class="icon" src="<?php echo get_template_directory_uri() ?>/img/en_detail/checklist.svg" alt="">
 			<img class="icon" src="<?php echo get_template_directory_uri() ?>/img/en_detail/camera.svg" alt="">
+			
 		</div>
 
 		<div class="container left">
 			<h3>A qui est destiné le BUT MMI ?</h3>
-			<p>Notre objectif est de composer une promotion avec :
-				-	50% de bacheliers technologiques, de préférence issus de la filière STI2D 
-				option SIN, avec un excellent dossier.
-				-	50% de bacheliers généraux, avec des options scientifiques.
-				Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture 
-				artistique et esthétique.</p>
+			<?php echo($detail_but['detail_but_forwho_content']); ?>
 			<img class="icon" src="<?php echo get_template_directory_uri() ?>/img/en_detail/code.svg" alt="">
 		</div>
 
 		<div class="container right">
 			<h3>Les projets</h3>
-			<p>Les projets sont au coeur de la formation MMI. Ils permettent aux étudiants de mettre 
-				en application pratique des savoirs théoriques. Grâce aux projets tuteurés, les étudiants 
-				peuvent présenter des réalisations de qualité pour leur devenir professionnel (stage,
-				alternance ou emploi).</p>
+			<?php echo($detail_but['detail_but_projects_content']); ?>
 		</div>
 		
-
-
-
-
 		<div id="years">
 			<h4>Première année</h4>
-			<p>Réalisation et mise en oeuvre d’une exposition multimédia : conception 
-				des oeuvres et des outils de communication relatifs à l’exposition. </p>
+			<?php echo($detail_but['detail_but_projects_years']['detail_but_projects_years_first']); ?>
 			<h4>Deuxième année</h4>
-			<p>Constitués en agences, les étudiants répondent à un besoin réel issu 
-				d’un client : création de sites web, conception de produits audiovisuels, 
-				stratégie de communication…</p>
+			<?php echo($detail_but['detail_but_projects_years']['detail_but_projects_years_second']); ?>
 			<h4>Troisième année</h4>
-			<p>Stage en entreprise et possibilité d'alternance</p>
+			<?php echo($detail_but['detail_but_projects_years']['detail_but_projects_years_third']); ?>
 		</div>
-
-
-
-
-
-
-
 
 		<div class="container left">
 			<h3>La spécialité de MMI Chambéry</h3>
-			<p>Notre objectif est de composer une promotion avec :
-				-	50% de bacheliers technologiques, de préférence issus de la filière STI2D 
-				option SIN, avec un excellent dossier.
-				-	50% de bacheliers généraux, avec des options scientifiques.
-				Dans l’idéal le candidat au BUT MMI aura également une ouverture à la culture 
-				artistique et esthétique.</p>
+			<?php echo($detail_but['detail_but_speciality_content']); ?>
 		</div>
 
 	</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	<section id="department">
 		<h2>Le département</h2>
